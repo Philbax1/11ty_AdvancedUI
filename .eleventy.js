5696954dcdf3f58a719987ac807bfcedee63a385
horrjd1 <jamie489horrell@gmail.com>
2020-08-14 15:09:43 +1200

    added bootstrap and navbar

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,3 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addFilter("filesize", function (path) {
-    return "0 KB";
-  });
+  eleventyConfig.addPassthroughCopy("_assets/css");
 };
